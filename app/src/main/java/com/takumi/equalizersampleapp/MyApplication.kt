96111646e818fcdc57f3fcf5ed5a98d6b1cae649
arsha reubro <arsha.reubro@gmail.com>
2023-01-25 19:45:18 +0530

    Initial commit

---
+++ app/src/main/java/com/takumi/equalizersampleapp/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.takumi.equalizersampleapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
---
+++ app/src/main/java/com/takumi/equalizersampleapp/data/kchange.kt
@@ -0,0 +1,20 @@
+package com.takumi.equalizersampleapp.data
+
+import java.text.DecimalFormat
+import java.text.NumberFormat
+
+fun formatValue(value: Double): String? {
+    var value = value
+    val power: Int
+    val suffix = " kmbt"
+    var formattedNumber = ""
+    val formatter: NumberFormat = DecimalFormat("#,###.#")
+    power = StrictMath.log10(value).toInt()
+    value = value / Math.pow(10.0, (power / 3 * 3).toDouble())
+    formattedNumber = formatter.format(value)
+    formattedNumber = formattedNumber + suffix[power / 3]
+    return if (formattedNumber.length > 4) formattedNumber.replace(
+        "\\.[0-9]+".toRegex(),
+        ""
+    ) else formattedNumber
+}
